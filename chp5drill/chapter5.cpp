11c47b13ec69cb188586b1ac9ddc9281fb759117
Asztalos Attila <asztalosattila007@gmail.com>
2020-10-10 22:52:53 +0200

    Chapter 5 drill

---
+++ chp5drill/chapter5.cpp
@@ -0,0 +1,48 @@
+#include "std_lib_facilities.h"
+int main()
+try {          
+// cout << "Success!\n"; 
+// cout << "Success!\n";
+ // cout << "Success" << "!\n";
+   // cout << "Success" << '\n';
+/*int res = 7; 
+vector<int> v(10); 
+v[5] = res; 
+cout << "Success!\n";
+keep_window_open();*/
+  /*vector<int> v(10); v(5) = 7; 
+  if (v(5)!=7) cout << "Success!\n";*/
+	/*int cond = 1;
+     if (cond==1) cout << "Success!\n"; else cout << "Fail!\n";*/
+ //bool c = false; if (c == false) cout << "Success!\n"; else cout << "Fail!\n";	
+	//string s = "ape"; string c = "fool"; if (s<c) cout << "Success!\n";
+	//string s = "ape"; if (s=="ape") cout << "Success!\n";
+    //string s = "ape"; if (s=="ape") cout << "Success!\n"; 
+    // string s = "ape"; if (s=="ape") cout << "Success!\n";
+    //vector<char> v(5); for (int i=0; i<v.size(); ++i); cout << "Success!\n";
+    //vector<char> v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+    //string s = "Success!\n"; for (int i=0; i<9; ++i) cout << s[i];
+    /* bool then = true;
+    if (then==true) cout << "Success!\n"; else cout << "Fail!\n";*/
+    //int x = 2000; char c = x; if (x==2000) cout << "Success!\n"; 
+    //string s = "Success!\n"; for (int i=0; i<10; ++i) cout << s[i];
+    // vector<int> v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+    // int i=0; int j = 9; while (i<2) ++i; if (j>i) cout << "Success!\n";
+    //int x = 2; double d = 4.5/(x/2); if (d==2*x+0.5) cout << "Success!\n";
+    //string s = "Success!\n"; for (int i=0; i<=9; ++i) cout << s[i];
+    //int i=0; int j=9; while (j<10) ++j; if (j>i) cout << "Success!\n";
+     //int x = 4; double d = 8/(x-2); if (d=2*x*0.5) cout << "Success!\n";
+	cout << "Success!\n";
+return 0;
+int cond =1;
+ if (cond = 1) cout << "Success!\n"; else cout << "Fail!\n";
+}
+catch (exception& e) { 
+cerr << "error: " << e.what() << '\n'; 
+keep_window_open(); 
+return 1;}
+catch (...) {          
+cerr << "Oops: unknown exception!\n";           
+keep_window_open();          
+return 2;
+}
